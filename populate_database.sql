7d1668c2c72f443456ccae2a0f338280772ffc2a
Camelia Modiga <camelia-maria.modiga@student.tuiasi.ro>
2021-11-06 19:35:53 +0200

    Add populate database script

---
+++ populate_database.sql
@@ -0,0 +1,9 @@
+insert into author (first_name,last_name) values ("Bogdan","Popa");
+insert into author (first_name,last_name) values ("Cristian","Vladimirescu");
+
+insert into book values ("ISBN 978-3-16-148410-0","Dosar permanent","Nemira",2019,"Autobiografii");
+insert into book values ("ISBN 978-6-06-430912-9 ","Minerva se dezlantuie","Nemira",2019,"Fictiune");
+insert into book values ("ISBN 978-3-16-148410-1","Stilet cu sampanie","Nemira",2020,"Fictiune");
+
+insert into book_author values("ISBN 978-3-16-148410-0",1,1);
+insert into book_author values("ISBN 978-3-16-148410-0",2,2);
